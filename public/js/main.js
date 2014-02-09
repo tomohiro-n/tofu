06261e878991baf895299b11afe5218fe0db3e27
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2014-02-09 23:06:22 +0900

    simple search function. fix #14

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
-function reloadRegistered() {
+function reloadRegistered(searchKey) {
 	var registeredWordsDom = $('#registered-words');
 	registeredWordsDom.html('');
-	for(var key in localStorage) {
+	for (var key in localStorage) {
+		if (searchKey) {
+			if (!key.match(new RegExp('^.*' + searchKey + '.*$'))) continue;
+		}
 		registeredWordsDom.append('<p>')
 		registeredWordsDom.append(key + ': ' + localStorage[key]);
 		registeredWordsDom.append('</p>')
-	};
+	}
 };
 
 function registerWord() {
@@ -13,4 +16,9 @@ function registerWord() {
 	var desc = $("#description").val();
 	localStorage[word] = desc;
 	reloadRegistered();
+};
+
+function searchWords() {
+	var searchKey = $("#searchWord");
+	reloadRegistered(searchKey);
 };
