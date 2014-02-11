7e4286d6d6598b06e351664bc1e899777fea39c4
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2014-02-12 00:51:45 +0900

    first, description is now updatable. refs #26

--- public/js/service/TofuService.js
+++ public/js/service/TofuService.js
@@ -24,10 +24,21 @@ Tofu.service.TofuService.reloadRegistered = function(searchKey, searchTag) {
 	}
 };
 
-Tofu.service.TofuService.registerWord = function() {
+Tofu.service.TofuService.registerNewWord = function() {
 	var word = $("#word").val();
 	var desc = $("#description").val();
 	var tags = $("#tags").val().split(" ");
+	Tofu.service.TofuService.registerWord(word, desc, tags);
+};
+
+Tofu.service.TofuService.updateWord = function() {
+	var word = $("#registered-word-name").text();
+	var desc = $("#registered-word-description-txt").val();
+	var tags = $("#registered-word-tags").text();
+	Tofu.service.TofuService.registerWord(word, desc, tags);
+}
+
+Tofu.service.TofuService.registerWord = function(word, desc, tags) {
 	localStorage[word] = JSON.stringify(new Tofu.model.Word(word, desc, tags));
 	Tofu.service.TofuService.reloadRegistered();
 };
@@ -41,7 +52,12 @@ Tofu.service.TofuService.searchWords = function() {
 Tofu.service.TofuService.showWordDetail = function(wordName) {
 	wordName = wordName.data; //FIXME
 	var data = JSON.parse(localStorage[wordName]);
-	$("#registered-word-name").text(wordName);
-	$("#registered-word-tags").text(data.tags);
-	$("#registered-word-description").text(data.description);
+	$("#registered-word-name").html(data.word);
+	$("#registered-word-tags").html(data.tags);
+	$("#registered-word-description").html(
+		'<input readonly="readonly" id="registered-word-description-txt" value="' +
+		data.description + '">');
+	$("#registered-word-description").dblclick(function(){
+		$("#registered-word-description-txt").attr('readonly', false);
+	});
 };
