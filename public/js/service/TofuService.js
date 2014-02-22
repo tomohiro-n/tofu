99c5ec2fef97c6779041f7871543458fcf75c12f
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2014-02-23 01:01:27 +0900

    replaced almost all logics with AngularJS. fixes #32

--- public/js/service/TofuService.js
+++ public/js/service/TofuService.js
@@ -4,6 +4,7 @@ Tofu.service.TofuService = angular.module('tofuControllers', []);
 
 Tofu.service.TofuService.controller('tofuController', function($scope) {
 	$scope.words = [];
+	$scope.wordReadOnlyMode = true;
 	for(var key in localStorage) {
 		if (key != undefined) $scope.words.push(JSON.parse(localStorage[key]));
 	}
@@ -18,29 +19,25 @@ Tofu.service.TofuService.controller('tofuController', function($scope) {
 		$scope.newWordDescription = '';
 		$scope.newWordTags = '';
 	};
+
+	$scope.showWordDetail = function(word) {
+		$scope.registeredWordName = word.word;
+		$scope.registeredWordDescription = word.description;
+		$scope.registeredWordTags = word.tags;
+	};
+
+	$scope.updateWord = function() {
+		var wordName = $scope.registeredWordName;
+		$scope.words = $scope.words.filter(function(word) {
+			return word.word != wordName;
+		}, this);
+		var word = new Tofu.model.Word(wordName, $scope.registeredWordDescription, $scope.registeredWordTags);
+		localStorage[$scope.registeredWordName] = JSON.stringify(word);
+		$scope.words.push(word);
+		$scope.wordReadOnlyMode = true;
+	};
+
+	$scope.editMode = function() {
+		$scope.wordReadOnlyMode = false;
+	};
 });
-
-Tofu.service.TofuService.updateWord = function() {
-	var word = $("#registered-word-name").text();
-	var desc = $("#registered-word-description-txt").val();
-	var tags = $("#registered-word-tags").text();
-	Tofu.service.TofuService.registerWord(word, desc, tags);
-}
-
-Tofu.service.TofuService.registerWord = function(word, desc, tags) {
-	localStorage[word] = JSON.stringify(new Tofu.model.Word(word, desc, tags));
-
-};
-
-Tofu.service.TofuService.showWordDetail = function(wordName) {
-	wordName = wordName.data; //FIXME
-	var data = JSON.parse(localStorage[wordName]);
-	$("#registered-word-name").html(data.word);
-	$("#registered-word-tags").html(data.tags);
-	$("#registered-word-description").html(
-		'<input readonly="readonly" id="registered-word-description-txt" value="' +
-		data.description + '">');
-	$("#registered-word-description").dblclick(function(){
-		$("#registered-word-description-txt").attr('readonly', false);
-	});
-};
